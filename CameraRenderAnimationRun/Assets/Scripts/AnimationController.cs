dc5c4fcff8b22f84093e6f13bc6b4ed300f2ce83
Mubasshir Ahmed <mubasshirahmed26@gmail.com>
2017-10-28 10:41:24 +0600

    some changes

--- CameraRenderAnimationRun/Assets/Scripts/AnimationController.cs
+++ CameraRenderAnimationRun/Assets/Scripts/AnimationController.cs
@@ -8,12 +8,15 @@ public class AnimationController : MonoBehaviour
     {
         print("Visible");
         //GetComponent<Animator>().enabled = true;
-        GetComponentInParent<Animator>().enabled = true;
+        //GetComponentInParent<Animator>().enabled = true;
+        gameObject.transform.parent.GetComponent<Animator>().enabled = true;
+
     }
     private void OnBecameInvisible()
     {
         print("Invisible");
         //GetComponent<Animator>().enabled = false;
-        GetComponentInParent<Animator>().enabled = false;
+        //GetComponentInParent<Animator>().enabled = false;
+        gameObject.transform.parent.GetComponent<Animator>().enabled = false;
     }
 }
